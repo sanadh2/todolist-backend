ffc9f94981bfe6683d18b3491485939a2f7b0595
Sanadh A N <136793235+sanadh2@users.noreply.github.com>
2023-11-29 20:07:49 +0530

    Update app.js 3

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:2222",
+    origin: "http://localhost:1111",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
