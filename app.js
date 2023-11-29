307bb1c680fe2fe914951963f3df34f0fd53b06e
Sanadh A N <136793235+sanadh2@users.noreply.github.com>
2023-11-29 19:45:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.use(morgan("dev"));
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options('*', cors());
 app.get("/", (req, res) => {
   return res.status(200).send("<h1>Welcome to my server, bitches</h1>");
 });
